057be4fd0d9b199a262d8c14c64d9992468075a6
Nima Mahmoudi <nima_mahmoudi@live.com>
2020-05-28 12:25:20 -0400

    cleaned up zeromq processing

--- examples/06-worker.py
+++ examples/06-worker.py
@@ -2,67 +2,11 @@ import zmq
 import time
 import sys
 
-from pacssim.SimProcess import ExpSimProcess
-from pacssim.FunctionInstance import FunctionInstance
-from pacssim.ServerlessTemporalSimulator import ServerlessTemporalSimulator
-
 from tqdm import tqdm
-import numpy as np
 import struct
 import multiprocessing
 
-cold_service_rate = 1/2.163
-warm_service_rate = 1/2.016
-expiration_threshold = 600
-
-arrival_rate = 0.9
-max_time = 300
-
-# number of simulations samples produced
-num_sim = 1000
-
-running_function_count = 3
-idle_function_count = 5
-
-cold_service_process = ExpSimProcess(rate=cold_service_rate)
-warm_service_process = ExpSimProcess(rate=warm_service_rate)
-
-def generate_trace():
-    idle_functions = []
-    for _ in range(idle_function_count):
-        f = FunctionInstance(0,
-                                cold_service_process,
-                                warm_service_process,
-                                expiration_threshold
-                                )
-
-        f.state = 'IDLE'
-        f.is_cold = False
-        # when will it be destroyed if no requests
-        f.next_termination = 300
-        # so that they would be less likely to be chosen by scheduler
-        f.creation_time = 0.01
-        idle_functions.append(f)
-
-    running_functions = []
-    for _ in range(running_function_count):
-        f = FunctionInstance(0,
-                                cold_service_process,
-                                warm_service_process,
-                                expiration_threshold
-                                )
-
-        f.state = 'IDLE'
-        f.is_cold = False
-        # transition it into running mode
-        f.arrival_transition(0)
-
-        running_functions.append(f)
-
-    sim = ServerlessTemporalSimulator(running_functions, idle_functions, arrival_rate=arrival_rate, warm_service_rate=warm_service_rate, cold_service_rate=cold_service_rate,
-                                        expiration_threshold=expiration_threshold, max_time=max_time)
-    sim.generate_trace(debug_print=False, progress=False)
-    return sim.get_cold_start_prob()
+from examples.sim_trace import generate_trace
 
 port = "5556"
 if len(sys.argv) > 1:
@@ -70,7 +14,7 @@ if len(sys.argv) > 1:
     int(port)
 socket_addr = "tcp://127.0.0.1:%s" % port
 
-worker_count = 9
+worker_count = multiprocessing.cpu_count() * 2 + 1
 stop_signal = False
 def worker(context=None, name="worker"):
     context = context or zmq.Context.instance()
---
+++ examples/sim_trace.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+from pacssim.SimProcess import ExpSimProcess
+from pacssim.FunctionInstance import FunctionInstance
+from pacssim.ServerlessTemporalSimulator import ServerlessTemporalSimulator
+
+cold_service_rate = 1/2.163
+warm_service_rate = 1/2.016
+expiration_threshold = 600
+
+arrival_rate = 0.9
+max_time = 300
+
+running_function_count = 3
+idle_function_count = 5
+
+cold_service_process = ExpSimProcess(rate=cold_service_rate)
+warm_service_process = ExpSimProcess(rate=warm_service_rate)
+
+def generate_trace():
+    idle_functions = []
+    for _ in range(idle_function_count):
+        f = FunctionInstance(0,
+                                cold_service_process,
+                                warm_service_process,
+                                expiration_threshold
+                                )
+
+        f.state = 'IDLE'
+        f.is_cold = False
+        # when will it be destroyed if no requests
+        f.next_termination = 300
+        # so that they would be less likely to be chosen by scheduler
+        f.creation_time = 0.01
+        idle_functions.append(f)
+
+    running_functions = []
+    for _ in range(running_function_count):
+        f = FunctionInstance(0,
+                                cold_service_process,
+                                warm_service_process,
+                                expiration_threshold
+                                )
+
+        f.state = 'IDLE'
+        f.is_cold = False
+        # transition it into running mode
+        f.arrival_transition(0)
+
+        running_functions.append(f)
+
+    sim = ServerlessTemporalSimulator(running_functions, idle_functions, arrival_rate=arrival_rate, warm_service_rate=warm_service_rate, cold_service_rate=cold_service_rate,
+                                        expiration_threshold=expiration_threshold, max_time=max_time)
+    sim.generate_trace(debug_print=False, progress=False)
+    return sim.get_cold_start_prob()
+
+if __name__ == "__main__":
+    print([generate_trace() for _ in range(10)])
