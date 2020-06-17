619a16ecabba447f7b42dba26bdf4044ee106b0e
Nima Mahmoudi <nima_mahmoudi@live.com>
2020-06-17 19:17:45 -0400

    added exponential serverless temporal simulator

--- pacssim/ServerlessTemporalSimulator.py
+++ pacssim/ServerlessTemporalSimulator.py
@@ -31,6 +31,67 @@ class ServerlessTemporalSimulator(ServerlessSimulator):
         self.servers = [*running_function_instances, *idle_function_instances]
 
 
+
+class ExponentialServerlessTemporalSimulator(ServerlessTemporalSimulator):
+    """ExponentialServerlessTemporalSimulator is a simulator assuming exponential distribution for proceesing times
+        which means each process is state-less and we can generate a service time and use that from now on.
+
+        running_function_instance_count is the number of instances currently processing a request, and 
+        idle_function_instance_next_terminations is an array of next termination scheduled for idle functions
+        if they receive no new requests.
+    """
+    def __init__(self, running_function_instance_count, idle_function_instance_next_terminations,
+                 arrival_process=None, warm_service_process=None,
+                 cold_service_process=None, expiration_threshold=600, max_time=24*60*60,
+                 maximum_concurrency=1000, **kwargs):
+
+        cold_service_process = ExpSimProcess(rate=cold_service_rate)
+        warm_service_process = ExpSimProcess(rate=warm_service_rate)
+
+        idle_functions = []
+        for next_term in idle_function_instance_next_terminations:
+            f = FunctionInstance(0,
+                                cold_service_process,
+                                warm_service_process,
+                                expiration_threshold
+                                )
+
+            f.state = 'IDLE'
+            f.is_cold = False
+            # when will it be destroyed if no requests
+            f.next_termination = next_term
+            # so that they would be less likely to be chosen by scheduler
+            f.creation_time = 0.01
+            idle_functions.append(f)
+
+        running_functions = []
+        for _ in range(running_function_instance_count):
+            f = FunctionInstance(0,
+                                cold_service_process,
+                                warm_service_process,
+                                expiration_threshold
+                                )
+
+            f.state = 'IDLE'
+            f.is_cold = False
+            # transition it into running mode
+            f.arrival_transition(0)
+
+            running_functions.append(f)
+
+        super().__init__(
+            running_function_instances=running_functions,
+            idle_function_instances=idle_functions,
+            arrival_process=arrival_process,
+            warm_service_process=warm_service_process,
+            cold_service_process=cold_service_process,
+            expiration_threshold=expiration_threshold,
+            max_time=max_time,
+            maximum_concurrency=maximum_concurrency,
+            **kwargs
+        )
+
+
 if __name__ == "__main__":
     from pacssim.SimProcess import ExpSimProcess
 
@@ -40,10 +101,10 @@ if __name__ == "__main__":
     expiration_threshold = 600
 
     arrival_rate = 0.9
-    max_time = 300
+    max_time = 600
 
     running_function_count = 3
-    idle_function_count = 5
+    idle_function_count = 10
 
     cold_service_process = ExpSimProcess(rate=cold_service_rate)
     warm_service_process = ExpSimProcess(rate=warm_service_rate)
@@ -81,5 +142,14 @@ if __name__ == "__main__":
 
     sim = ServerlessTemporalSimulator(running_functions, idle_functions, arrival_rate=arrival_rate, warm_service_rate=warm_service_rate, cold_service_rate=cold_service_rate,
                                       expiration_threshold=expiration_threshold, max_time=max_time)
-    sim.generate_trace(debug_print=True, progress=True)
+    sim.generate_trace(debug_print=False, progress=True)
+    sim.print_trace_results()
+
+
+    print("Testing out the new functionality added.")
+    idle_function_instance_next_terminations = [300] * idle_function_count
+    sim = ExponentialServerlessTemporalSimulator(running_function_count, idle_function_instance_next_terminations, arrival_rate=arrival_rate, 
+                                        warm_service_rate=warm_service_rate, cold_service_rate=cold_service_rate,
+                                        expiration_threshold=expiration_threshold, max_time=max_time)
+    sim.generate_trace(debug_print=False, progress=True)
     sim.print_trace_results()
