0360a8dea8001bd4fb1f79fa92e7cbfe1cde43a2
Nima Mahmoudi <nima_mahmoudi@live.com>
2020-07-02 19:23:36 -0400

    changed docker workflow

--- api/build-run.sh
+++ api/build-run.sh
@@ -1,3 +1,6 @@
 IMAGE_NAME=nimamahmoudi/simfaas-api
+
+cp -R ../simfaas ./simfaas
 docker build -t $IMAGE_NAME:latest -f Dockerfile .
 docker run -it --rm -p 5000:5000 --name simfaasapi $IMAGE_NAME
+rm -R simfaas
