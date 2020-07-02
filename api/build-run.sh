b66582066e89c1b788d91390efa82edc8f525072
Nima Mahmoudi <nima_mahmoudi@live.com>
2020-07-02 19:33:45 -0400

    Update build-run.sh

--- api/build-run.sh
+++ api/build-run.sh
@@ -1,6 +1,6 @@
 IMAGE_NAME=nimamahmoudi/simfaas-api
 
-cp -R ../simfaas ./simfaas
+cp -R ../ ./simfaas
 docker build -t $IMAGE_NAME:latest -f Dockerfile .
 docker run -it --rm -p 5000:5000 --name simfaasapi $IMAGE_NAME
 rm -R simfaas
